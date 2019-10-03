3d1b85bfd54827935799b0bcbbb0249ab2ff0934
Stanley Louie <stylouie123@gmail.com>
2019-10-03 16:27:44 -0400

    did some html stuff

--- src/server.js
+++ src/server.js
@@ -1,15 +1,44 @@
 const http = require('http');
 const url = require('url');
 const query = require('querystring');
-// const htmlHandler = require('./htmlResponses.js');
-// const jsonHandler = require('./jsonResponses.js');
+const htmlHandler = require('./htmlResponses.js');
+const jsonHandler = require('./jsonResponses.js');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
-// const onRequest = (request, response) => {
 
-// };
+// key:value object to look up URL routes to specific functions
+const urlStruct = {
+  '/': htmlHandler.getIndex,
+  '/style.css': htmlHandler.getCSS,
+  '/success': jsonHandler.success,
+  '/badRequest': jsonHandler.badRequest,
+  '/getUsers': jsonHandler.getUsers,
+  '/addUsers': jsonHandler.addUsers,
+  '/notReal': jsonHandler.notReal,
+  '/updated': jsonHandler.updated,
+  notFound: jsonHandler.notFound,
+};
 
-// http.createServer(onRequest).listen(port);
+const onRequest = (request, response) => {
+// parse the url using the url module
+  // This will let us grab any section of the URL by name
+  const parsedUrl = url.parse(request.url);
+
+  // grab the query parameters (?key=value&key2=value2&etc=etc)
+  // and parse them into a reusable object by field name
+  const params = query.parse(parsedUrl.query);
+
+
+  // check if the path name (the /name part of the url) matches
+  // any in our url object. If so call that function. If not, default to index.
+  if (urlStruct[parsedUrl.pathname]) {
+    urlStruct[parsedUrl.pathname](request, response, params);
+  } else {
+    urlStruct.notFound(request, response, params);
+  }
+};
+
+http.createServer(onRequest).listen(port);
 
 console.log(`Listening on 127.0.0.1: ${port}`);
